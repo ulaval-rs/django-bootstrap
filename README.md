c7ecfda9973250925809e83335588912289fdeea
olichose123 <oli.banville@gmail.com>
2021-06-10 09:21:09 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bootstrap Django
+A simple, empty django project with sane defaults to get you started
---
+++ backend/api/settings.py
@@ -0,0 +1,139 @@
+from pathlib import Path
+import os
+import datetime
+import distutils.util
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = bool(distutils.util.strtobool(os.environ.get("DEBUG", "no")))
+
+ALLOWED_HOSTS = ["*"]
+
+DJANGO_APPS = [
+    "django_db_prefix",  # order-specific third-party app
+    "admin_interface",  # order-specific third-party app
+    "colorfield",  # order-specific third-party app
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.forms",
+]
+
+THIRD_PARTY_APPS = [
+    "django_extensions",
+    "django_filters",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "django_q",
+    "jsoneditor",
+]
+
+LOCAL_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "api.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "api.wsgi.application"
+
+# TODO: change this to fit your project
+DB_PREFIX = "api_"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    },
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# Email
+SERVER_EMAIL = "su@email.com"
+DEFAULT_FROM_EMAIL = "webmaster@localhost"
+EMAIL_SUBJECT_PREFIX = "[Django] "
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 25
+
+# Language and time
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "America/Toronto"
+USE_TZ = True
+USE_THOUSAND_SEPARATOR = True
+THOUSAND_SEPARATOR = " "
+
+
+# Storage, static and media
+DEFAULT_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
+STATIC_URL = "/static/"
+STATIC_ROOT = "/usr/src/static/"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "usr/src/media/"
+
+
+# Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{os.environ.get('REDIS_HOST')}:{os.environ.get('REDIS_PORT')}",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+# REST_FRAMEWORK configuration
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
+}
